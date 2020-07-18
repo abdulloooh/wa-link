5469964f0b1834c391650de0c1adb8581807310c
abdulloooh <abdullahakinwumi@gmail.com>
2020-07-18 19:51:05 +0100

    installed express and configured server for deployment on heroku

---
+++ build/precache-manifest.14bad70323f9df3e086ef92f7901ef6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d3dbc12a610161c22c4efb328f79c03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb57790a4ff9b36e7368",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "5a4fa122914ceac68579",
+    "url": "/static/css/main.e3400931.chunk.css"
+  },
+  {
+    "revision": "cb57790a4ff9b36e7368",
+    "url": "/static/js/2.849309f0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.849309f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a4fa122914ceac68579",
+    "url": "/static/js/main.c239ace9.chunk.js"
+  },
+  {
+    "revision": "0ce4fade1c67d0bc3696",
+    "url": "/static/js/runtime-main.3e089d29.js"
+  }
+]);
