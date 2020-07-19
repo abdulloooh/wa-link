bb1dcf9d816865caf436c4143286d0acd0a0781d
abdulloooh <abdullahakinwumi@gmail.com>
2020-07-19 10:56:14 +0100

    Updates

---
+++ precache-manifest.3d1d8ffa224e8efbbb512472feca9012.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e71abf20a3d3f2016a33203dba8d83d7",
+    "url": "/wa-link/index.html"
+  },
+  {
+    "revision": "cb57790a4ff9b36e7368",
+    "url": "/wa-link/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "2b8439af89ac3b3b3930",
+    "url": "/wa-link/static/css/main.7ff033c1.chunk.css"
+  },
+  {
+    "revision": "cb57790a4ff9b36e7368",
+    "url": "/wa-link/static/js/2.849309f0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/wa-link/static/js/2.849309f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b8439af89ac3b3b3930",
+    "url": "/wa-link/static/js/main.3fc69986.chunk.js"
+  },
+  {
+    "revision": "af0a43e26f8e7ca7d616",
+    "url": "/wa-link/static/js/runtime-main.90cb027b.js"
+  }
+]);
